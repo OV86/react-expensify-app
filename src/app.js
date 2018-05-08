971690560d711ad7e861bf6c1addf92bdb69ead3
Olaf Vaher <olafvaher@Olafs-MacBook-Pro.local>
2018-05-08 10:57:04 +0200

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -4,21 +4,13 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-// import { setTextFilter } from './actions/filters';
+import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css'; // needs to be imported before own styles. Makes the website look same in all browsers. Resets default styles.
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 300, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 10950 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
