ae713a8296b5197ffcfb9e52b933896af4a88af5
Olaf Vaher <olafvaher@Olafs-MacBook-Pro.local>
2018-05-08 13:29:19 +0200

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+// import { addExpense } from './actions/expenses';
+// import { setTextFilter } from './actions/filters';
+// import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css'; // needs to be imported before own styles. Makes the website look same in all browsers. Resets default styles.
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
