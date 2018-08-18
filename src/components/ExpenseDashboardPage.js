96d26cc43ffe65d4d0a1271f15a12dc645cb9f8b
Olaf Vaher <olafvaher@Olafs-MacBook-Pro.local>
2018-08-18 17:59:29 +0200

    Fix typo

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,4 +1,4 @@
-wimport React from 'react';
+import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
 import ExpensesSummary from './ExpensesSummary';
